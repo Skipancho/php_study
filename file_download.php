7865d12f5ab7bcf4a5886f1877dc6592a1c4d08d
Skipancho <79133730+Skipancho@users.noreply.github.com>
2021-12-10 14:51:33 +0900

    Create file_download.php

---
+++ file_download.php
@@ -0,0 +1,23 @@
+<?php
+  $filename = $_GET["filename"];
+	$target_dir = $_GET["target_dir"];
+
+	$file = $_SERVER['DOCUMENT_ROOT']."/".$target_dir."/".$filename;
+	$filesize = filesize($file);
+	if (is_file($file)) {
+		header("Content-type: application/octet-stream"); 
+        header("Content-Length: ".filesize("$file"));
+        header("Content-Disposition: attachment; filename=$filename");
+        header("Content-Transfer-Encoding: binary"); 
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Pragma: public"); 
+        header("Expires: 0"); 
+
+        $fp = fopen($file, "r"); 
+	    if(!fpassthru($fp)){
+	 	   	fclose($fp);
+	    }	    
+    }else {
+    	echo "<script>alert(".$file." not found);</script>";
+    }
+?>
